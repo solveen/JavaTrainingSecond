102ad6980d730e23732bfc3684e48de0d2425da9
Solveen Gurung <solveengurung@gmail.com>
2022-03-15 18:18:34 +0545

    Cookies and RequestDispatcher

--- src/ServletPractice/ServletPractice.java
+++ src/ServletPractice/ServletPractice.java
@@ -1,7 +1,10 @@
 package ServletPractice;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,26 +14,63 @@ import java.io.PrintWriter;
 @WebServlet(name = "ServletPractice", urlPatterns = "/login")
 public class ServletPractice extends HttpServlet {
 
-    public void init(){
+    private String message;
 
+    public void init() {
+        message = "Hello World!";
     }
 
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-        response.setContentType("web/index.html");
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //yo folder ko directory haina hai
+        response.setContentType("text/html");
+
         String email = request.getParameter("email");
         //name used inside the input tag
         String password = request.getParameter("password");
-        if(password.isEmpty()){
-            System.out.println("Please enter your password");
+
+        PrintWriter printWriter = response.getWriter();
+//
+//        printWriter.println(ck.getValue());
+//        printWriter.println("<form action='cookieTest' method = 'POST' >");
+//        printWriter.println("<input type='submit' value = 'login' />");
+//        printWriter.println("</form>");
+
+
+
+        if (password.equals("123") && email.equals("admin@gmail.com")) {
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("Dashboard.jsp");
+            //             This creates cookies
+            Cookie ck = new Cookie("email", email);
+            response.addCookie(ck);
+            System.out.println(ck.getValue());
+            Cookie ckpw = new Cookie("password", password);
+            response.addCookie(ckpw);
+            System.out.println(ckpw.getValue());
+            //request dispatcher tala hunu parxa natra cookie store hudaina
+            requestDispatcher.forward(request, response);
+
+        } else {
+
+            printWriter.println("Incorrect Username or Password.");
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("index.jsp");
+            requestDispatcher.include(request, response);
+
         }
-
-        System.out.println("Your email is " + email + " Your password is: " + password);
-
+//
+//        System.out.println("Your email is " + email + " Your password is: " + password);
+        printWriter.close();
     }
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 
-    }
-    public void doDestroy(){
+    public void doDestroy() {
 
     }
 }
